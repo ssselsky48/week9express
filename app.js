ad59ca16e6a4d6f80428cce2cf94a1a3e9154c1b
Dmitry Rodionov <60671160+ssselsky48@users.noreply.github.com>
2021-05-12 20:19:24 +0300

    Update app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ export default (express, bodyParser, createReadStream, writeFileSync, crypto, ht
     .use(bodyParser.json())
 
     .all('/login/', (req, res) => {
-        res.end("nikird");
+        res.end("d_rdnv");
     })
 
     .all('/code/', (req, res) => {
@@ -121,7 +121,7 @@ export default (express, bodyParser, createReadStream, writeFileSync, crypto, ht
     })
 
     .all('/*', (req, res) => {
-        res.end("nikird");
+        res.end("d_rdnv");
     })
 
     return app;
