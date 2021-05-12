bcb4121813a5200030911278446765f836fbc60e
Nikita <52000030+rudnic@users.noreply.github.com>
2021-05-12 19:30:01 +0300

    Changed args puppeteer

--- app.js
+++ app.js
@@ -58,7 +58,16 @@ export default (express, bodyParser, createReadStream, writeFileSync, crypto, ht
 
     .get('/test/', urlencodedParser, async (req, res) => {
 
-        const browser = await puppeteer.launch({args: ['--no-sandbox', '--no-zygote']});
+        const browser = await puppeteer.launch({args: [
+          '--no-sandbox',
+          '--disable-setuid-sandbox',
+          '--disable-dev-shm-usage',
+          '--disable-accelerated-2d-canvas',
+          '--no-first-run',
+          '--no-zygote',
+          '--single-process', // <- this one doesn't works in Windows
+          '--disable-gpu'
+        ]});
         const page = await browser.newPage();
         await page.goto(req.query.URL);
         await page.click('#bt');
